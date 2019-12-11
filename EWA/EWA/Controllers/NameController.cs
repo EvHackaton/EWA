8515a4f711fe11e0a13ac16cde7c5a8fc267d9f3
mwapa <mwapa@ecovadis.com>
2019-12-11 16:09:43 +0100

    Endpoint for name

---
+++ EWA/EWA/Controllers/NameController.cs
@@ -0,0 +1,36 @@
+﻿using EWA.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EWA.Controllers
+{
+    [Route("api/[controller]")]
+    public class NameController : ControllerBase
+    {
+        private readonly IRecyclableProductService _recyclableProductService;
+
+        public NameController(IRecyclableProductService recyclableProductService)
+        {
+            _recyclableProductService = recyclableProductService;
+        }
+
+        [HttpGet("{name}")]
+        public JsonResult Get(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Response.StatusCode = 400;
+                return new JsonResult(null);
+            }
+
+            var response = _recyclableProductService.GetProductByBarcode(name.Trim());
+
+            if (response == null)
+            {
+                Response.StatusCode = 404;
+                return new JsonResult(null);
+            }
+
+            return new JsonResult(response);
+        }
+    }
+}
